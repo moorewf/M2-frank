1eadcdc7bcce433caf60112254b1b779e489049d
Mike Stillman <mikestillman1@gmail.com>
2022-07-20 15:18:48 -0400

    first commit for performant sparse matrices over finite fields

---
+++ M2/Macaulay2/e/SparseMatrixZZp.hpp
@@ -0,0 +1,155 @@
+#ifndef _sparse_matrix_ZZp_hpp_
+#define _sparse_matrix_ZZp_hpp_
+
+#include <vector>
+#include <tuple>
+#include <iostream>
+#include "aring-zzp-flint.hpp"
+
+using ZZpElement = M2::ARingZZpFlint::ElementType; // reallt just long.
+
+class SparseMatrixZZpConstRowIterator;
+
+class SparseMatrixZZp
+{
+  friend class SparseMatrixZZpConstRowIterator;
+public:
+  long numRows() const { return mNumRows; }
+  long numColumns() const { return mNumColumns; }
+
+  SparseMatrixZZp(const M2::ARingZZpFlint& F,
+                  long nrows,
+                  long ncols,
+                  const std::vector<std::tuple<long,long,ZZpElement>>& triples);
+
+  SparseMatrixZZpConstRowIterator cbegin(int row) const;
+  SparseMatrixZZpConstRowIterator cend(int row) const;
+
+  void dump(std::ostream &o) const;
+
+  void denseDisplay(std::ostream& o) const;
+private:
+  // Let e be the number of non-zero elements in the matrix
+  const M2::ARingZZpFlint& mField;
+  long mNumRows;
+  long mNumColumns;
+  std::vector<ZZpElement> mNonzeroElements; // 0..e-1
+  std::vector<long> mColumns; // 0..e-1: mColumns[i] is the column number of of the i-th element in mNonzeroElements.
+  std::vector<long> mRows; // 0..mNumRows // mRows[r]..mRows[r+1]-1 are the indices into mNonzeroElements, in this row.
+  // Note: if none, mRows[r-1] == mRows[r].
+
+  // Question: what is the format of a 0x0 matrix.
+  // [[], [], [0]]
+};
+
+class SparseMatrixZZpConstRowIterator
+{
+private:
+  const SparseMatrixZZp& mMatrix;
+  long mLoc;
+public:
+  SparseMatrixZZpConstRowIterator(const SparseMatrixZZp& A, long r)
+    : mMatrix(A),
+      mLoc(A.mRows[r])
+  {
+  }
+
+  SparseMatrixZZpConstRowIterator(const SparseMatrixZZp& A, long r, int this_is_the_end_constructor)
+    : mMatrix(A),
+      mLoc(A.mRows[r+1])
+  {
+  }
+
+  bool operator==(SparseMatrixZZpConstRowIterator j)
+  {
+    //TODO assert or test: same row, same matrix.
+    return(mLoc == j.mLoc);
+  }
+
+  bool operator!=(SparseMatrixZZpConstRowIterator j)
+  {
+    //TODO assert or test: same row, same matrix.
+    return(mLoc != j.mLoc);
+  }
+  
+  SparseMatrixZZpConstRowIterator operator++()
+  {
+    mLoc++;
+    return *this;
+  }
+
+  // operator*: returns (column, (nonzero) entry at given row and this column)
+  std::pair<long, ZZpElement> operator*()
+  {
+    return {mMatrix.mColumns[mLoc], mMatrix.mNonzeroElements[mLoc]};
+  }
+};
+
+inline SparseMatrixZZpConstRowIterator SparseMatrixZZp::cbegin(int row) const {
+  return SparseMatrixZZpConstRowIterator(*this, row);
+}
+
+inline SparseMatrixZZpConstRowIterator SparseMatrixZZp::cend(int row) const {
+  return SparseMatrixZZpConstRowIterator(*this, row, 0);
+}
+
+#if 0
+
+class Permutation
+{
+public:
+  // Operations:
+private:
+  std::vector<long> mPermutation;
+};
+
+class SparseVectorZZp
+{
+public:
+  std::vector<long> mComponents;
+  std::vector<ZZpElement> mElements;
+  // or:
+  std::vector<std::pair<long, ZZpElement>> mVector;
+};
+
+
+// TODO: also create a hash table class (r,c) => value.
+class SparseMatrixTripleZZp
+{
+public:
+  explicit operator(SparseMatrixZZp&);
+  long numRows() const { return mNumRows; }
+  long numColumns() const { return mNumColumns; }
+private:
+  // Let e be the number of non-zero elements in the matrix
+  long mNumRows;
+  long mNumColumns;
+
+  // Should the following be a stdd::vector<struct of 3 items> ?
+  std::vector<ZZpElement> mNonzeroElements; // 0..e-1
+  std::vector<long> mRows; // 0..e-1
+  std::vector<long> mColumns; // 0..e-1
+};
+
+
+namespace SparseMatrixOps
+{
+  // Constructors, change of representation
+  // read/write to/from file
+
+  // Transpose
+  // Matrix/Vector Multiplication (v*A, A*B, ...)
+
+  // sparse PLUQ decomposition:
+  // Wiedemann
+  // Lanczos
+  // rank computation
+
+  // finding a good matching...
+};
+#endif
+#endif
+
+// Local Variables:
+// indent-tabs-mode: nil
+// End:
---
+++ M2/Macaulay2/e/unit-tests/TestSparseMatrixZZp.cpp
@@ -0,0 +1,58 @@
+#include "SparseMatrixZZp.hpp"
+#include <iostream>
+#include <gtest/gtest.h>
+
+TEST(SparseMatrixZZp, fromTriple)
+{
+  M2:: ARingZZpFlint F(101);
+  SparseMatrixZZp A(F, 5, 7, {{0,1,1}, {0,4,2}, {0,6,3},
+                                {1,3,4},
+                                {2,0,5}, {2,2,6}, {2,6,7},
+                                {4,0,8}});
+
+  A.dump(std::cout);
+  
+  for (auto r=0; r < A.numRows(); ++r)
+    {
+      for (auto i = A.cbegin(r); i != A.cend(r); ++i)
+        {
+          auto t = *i;
+          std::cout << "[" << r << "," << t.first << "," << t.second << "] ";
+        }
+      std::cout << std::endl;
+    }
+}
+
+TEST(SparseMatrixZZp, zeroMatrix)
+{
+  M2:: ARingZZpFlint F(101);
+  SparseMatrixZZp A(F, 0, 0, {{}});
+
+  for (auto r=0; r < A.numRows(); ++r)
+    {
+      for (auto i = A.cbegin(r); i != A.cend(r); ++i)
+        {
+          auto t = *i;
+          std::cout << "[" << r << "," << t.first << "," << t.second << "] ";
+        }
+      std::cout << std::endl;
+    }
+}
+
+TEST(SparseMatrixZZp, zeroEntries)
+{
+  M2:: ARingZZpFlint F(101);
+  SparseMatrixZZp A(F, 3, 5, {});
+
+  A.dump(std::cout);
+  
+  for (auto r=0; r < A.numRows(); ++r)
+    {
+      for (auto i = A.cbegin(r); i != A.cend(r); ++i)
+        {
+          auto t = *i;
+          std::cout << "[" << r << "," << t.first << "," << t.second << "] ";
+        }
+      std::cout << std::endl;
+    }
+}
