a6161d0710bcb606ad70c89ab181a6ead93ec81b
Mike Stillman <mikestillman1@gmail.com>
2022-07-22 11:44:00 -0400

    added in denseDisplay, transpose

--- M2/Macaulay2/e/unit-tests/TestSparseMatrixZZp.cpp
+++ M2/Macaulay2/e/unit-tests/TestSparseMatrixZZp.cpp
@@ -6,10 +6,9 @@ TEST(SparseMatrixZZp, fromTriple)
 {
   M2:: ARingZZpFlint F(101);
   SparseMatrixZZp A(F, 5, 7, {{0,1,1}, {0,4,2}, {0,6,3},
-                                {1,3,4},
-                                {2,0,5}, {2,2,6}, {2,6,7},
-                                {4,0,8}});
-
+                           {1,3,4},
+                           {2,0,5}, {2,2,6}, {2,6,7},
+                           {4,0,8}});
   A.dump(std::cout);
   
   for (auto r=0; r < A.numRows(); ++r)
@@ -21,6 +20,8 @@ TEST(SparseMatrixZZp, fromTriple)
         }
       std::cout << std::endl;
     }
+
+  A.denseDisplay(std::cout);
 }
 
 TEST(SparseMatrixZZp, zeroMatrix)
@@ -55,4 +56,16 @@ TEST(SparseMatrixZZp, zeroEntries)
         }
       std::cout << std::endl;
     }
+}
+
+TEST(SparseMatrixZZp, transpose)
+{
+  M2:: ARingZZpFlint F(101);
+  SparseMatrixZZp A(F, 5, 7, {{0,1,1}, {0,4,2}, {0,6,3},
+                           {1,3,4},
+                           {2,0,5}, {2,2,6}, {2,6,7},
+                           {4,0,8}});
+
+  SparseMatrixZZp B = A.transpose();
+  B.denseDisplay(std::cout);
 }
