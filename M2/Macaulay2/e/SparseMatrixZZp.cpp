6e9464c83ca49aee9022bef249158dae55693d69
Mike Stillman <mikestillman1@gmail.com>
2022-07-22 15:36:12 -0400

    with Frank, added constructing from unsorted triples and constructing random matrices

--- M2/Macaulay2/e/SparseMatrixZZp.cpp
+++ M2/Macaulay2/e/SparseMatrixZZp.cpp
@@ -1,5 +1,10 @@
 #include "SparseMatrixZZp.hpp"
-#include <iomanip>
+#include "interface/random.h"
+#include "timing.hpp"
+
+#include <iomanip> // for std::setw
+#include <algorithm> // for std::sort
+#include <numeric> // for std::iota
 // example format
 // . 1 . . 2 . 3
 // . . . 4 . . .
@@ -33,13 +38,28 @@ SparseMatrixZZp::SparseMatrixZZp(const M2::ARingZZpFlint& field,
 {
   // Assume for now that the data is in row major order: first row, then the second, etc.
 
+  auto t1 = now();
+  std::vector<long> indices(triples.size()); // set the size of the vector.
+  std::iota(indices.begin(), indices.end(), 0); // fill it with 0..#triples-1
+  std::sort(indices.begin(), indices.end(), [&triples](long i, long j) {
+    auto i1 = std::get<0>(triples[i]);
+    auto i2 = std::get<1>(triples[i]);
+    auto j1 = std::get<0>(triples[j]);
+    auto j2 = std::get<1>(triples[j]);
+    return i1 < j1 or (i1 == j1 and i2 < j2);
+      });
+  std::cout << "sort time: " << seconds(now() - t1) << std::endl;
+
+  t1 = now();
+  // auto sorted_triples = triples;
+  // std::sort(sorted_triples.begin(), sorted_triples.end());
   long last_r = 0;
   mRows.push_back(0);
-  for (auto t : triples)
+  for (auto t : indices)
     {
-      long r = std::get<0>(t);
-      long c = std::get<1>(t);
-      long v = std::get<2>(t);
+      long r = std::get<0>(triples[t]);
+      long c = std::get<1>(triples[t]);
+      long v = std::get<2>(triples[t]);
       if (r > last_r) mRows.insert(mRows.end(), r-last_r, mColumns.size());
       last_r = r;
       mNonzeroElements.push_back(v);
@@ -47,6 +67,7 @@ SparseMatrixZZp::SparseMatrixZZp(const M2::ARingZZpFlint& field,
     }
   mRows.insert(mRows.end(), nrows-last_r, mColumns.size());
   mRows.push_back(mColumns.size());
+  std::cout << "construct time: " << seconds(now() - t1) << std::endl;
 }
 
 void SparseMatrixZZp::dump(std::ostream& o) const
@@ -105,17 +126,16 @@ SparseMatrixZZp SparseMatrixZZp::transpose() const
       work[mColumns[e]]++;
     }
 
-  // Now compute partial sums (but starting 0, a0, a0+a1, ...) into w, result->mColumns.
+  // Now compute partial sums (but starting 0, a0, a0+a1, ...) into w, result.mColumns.
+  // We also change work to have these partial sums as well.
   long sum = 0;
   for (long c = 0; c < numColumns(); ++c)
     {
       result.mRows[c] = sum;
       sum += work[c];
+      work[c] = result.mRows[c];
     }
   result.mRows[numColumns()] = sum;
-
-  for (long c = 0; c < numColumns(); ++c)
-    work[c] = result.mRows[c]; // These are now the next locations for elements in the transposed row.
 
   // Now we loop through all rows of original matrix, and for each column in that row
   // We will set the column index of the transposed element, and the entry itself.
@@ -125,11 +145,40 @@ SparseMatrixZZp SparseMatrixZZp::transpose() const
         long oldloc = ic;
         long newloc = work[mColumns[ic]]++; // new location, and bump next location for this column
         result.mNonzeroElements[newloc] = mNonzeroElements[oldloc];
+        //TODO: use field().set(...) instead...
         result.mColumns[newloc] = r;
       }
 
+  delete[] work;
   return result;
 }
+
+SparseMatrixZZp SparseMatrixZZp::randomSparseMatrix(const M2::ARingZZpFlint& F,
+                                                    long nrows,
+                                                    long ncols,
+                                                    float density)
+{
+  long nentries = nrows * 1.0 * ncols * density;
+  auto t1 = now();
+  std::vector<std::tuple<long,long,ZZpElement>> triples(nentries);
+  std::generate(triples.begin(),triples.end(), [&F,nrows,ncols]() {
+    ZZpElement val;
+    do
+      F.random(val);
+    while (F.is_zero(val));
+    return std::tuple<long,long,ZZpElement>(
+      rawRandomULong(nrows),
+      rawRandomULong(ncols),
+      val);
+  });
+  auto t2 = now();
+  auto result = SparseMatrixZZp(F, nrows, ncols, triples);
+  auto t3 = now();
+  std::cout << "time for generate: " << seconds(t2-t1) << std::endl;
+  std::cout << "time for constructor: " << seconds(t3-t2) << std::endl;
+  return result;
+}
+
 // Local Variables:
 // indent-tabs-mode: nil
 // End:
--- M2/Macaulay2/e/unit-tests/TestSparseMatrixZZp.cpp
+++ M2/Macaulay2/e/unit-tests/TestSparseMatrixZZp.cpp
@@ -1,4 +1,6 @@
 #include "SparseMatrixZZp.hpp"
+#include "timing.hpp"
+
 #include <iostream>
 #include <gtest/gtest.h>
 
@@ -9,6 +11,28 @@ TEST(SparseMatrixZZp, fromTriple)
                            {1,3,4},
                            {2,0,5}, {2,2,6}, {2,6,7},
                            {4,0,8}});
+  A.dump(std::cout);
+  
+  for (auto r=0; r < A.numRows(); ++r)
+    {
+      for (auto i = A.cbegin(r); i != A.cend(r); ++i)
+        {
+          auto t = *i;
+          std::cout << "[" << r << "," << t.first << "," << t.second << "] ";
+        }
+      std::cout << std::endl;
+    }
+
+  A.denseDisplay(std::cout);
+}
+
+TEST(SparseMatrixZZp, fromUnsortedTriple)
+{
+  M2:: ARingZZpFlint F(101);
+  SparseMatrixZZp A(F, 5, 7, {{0,6,3},
+                           {1,3,4}, {2,6,7},
+                           {2,0,5}, {2,2,6}, {0,4,2}, 
+                           {0,1,1}, {4,0,8}});
   A.dump(std::cout);
   
   for (auto r=0; r < A.numRows(); ++r)
@@ -66,6 +90,29 @@ TEST(SparseMatrixZZp, transpose)
                            {2,0,5}, {2,2,6}, {2,6,7},
                            {4,0,8}});
 
+  A.denseDisplay(std::cout);
+
+  std::cout << "Now for the transpose:" << std::endl;
   SparseMatrixZZp B = A.transpose();
   B.denseDisplay(std::cout);
+}
+
+TEST(SparseMatrixZZp, randomSparseMatrix)
+{
+  M2:: ARingZZpFlint F(101);
+  auto A = SparseMatrixZZp::randomSparseMatrix(F, 50, 60, .1);
+  A.denseDisplay(std::cout);
+  auto B = A.transpose();
+  std::cout << "and its transpose is: " << std::endl;
+  B.denseDisplay(std::cout);
+}
+
+TEST(SparseMatrixZZp, bigRandomSparseMatrix)
+{
+  M2:: ARingZZpFlint F(101);
+  auto A = SparseMatrixZZp::randomSparseMatrix(F, 50000, 60000, .01);
+  std::cout << "and now lets work on the transpose" << std::endl;
+  auto t1 = now();
+  auto B = A.transpose();
+  std::cout << "transose time: " << seconds(now() - t1) << std::endl;
 }
