fec6db0d7ce17e625b95d6b6c1d3d3b89a1fd5a1
moorewf <moorewf@wfu.edu>
2022-12-18 20:07:44 -0500

    Removed some more dead code.

--- M2/Macaulay2/e/sparse-matrix/PivotGraph.cpp
+++ M2/Macaulay2/e/sparse-matrix/PivotGraph.cpp
@@ -15,39 +15,25 @@ void PivotGraph::initialize(const std::vector<Edge>& edges)
 
 PivotGraph::PivotGraph(const SparseMatrixZZp& A,
 		       const PivotHelper& pivotHelper)
-  : mAdjacency(pivotHelper.numPivots(),std::vector<VertexType> {})
+  : mAdjacency(pivotHelper.numPivots(),
+	       std::vector<VertexType> {})
 {
    std::vector<Edge> edges;
-   std::cout << "Determining graph: " << std::endl;
-   // this seems to be where almost all time is spent...
-   //   need to do this faster.  in order to do that, we need
-   //   a way to determine which pivot index (in pivotHelper) a given (r,c) pivot pair corresponds to.
-   // for (int i = 0; i < pivotHelper.numPivots(); ++i)
-   // {
-   //    for (int j = i+1; j < pivotHelper.numPivots(); ++j)
-   //    {
-   //       if (A.entryPresent(pivotHelper.pivotRow(i),pivotHelper.pivotColumn(j)))
-   // 	    edges.push_back(std::make_pair(i,j));
-   //       if (A.entryPresent(pivotHelper.pivotRow(j),pivotHelper.pivotColumn(i)))
-   // 	    edges.push_back(std::make_pair(j,i));
-   //    }
-   // }
 
    for (VertexType i = 0; i < pivotHelper.numPivots(); ++i)
    {
-      auto begin = A.cbegin(pivotHelper.pivotRow(i));
-      auto end = A.cend(pivotHelper.pivotRow(i));
+      auto begin = A.cbeginColumns(pivotHelper.pivotRow(i));
+      auto end = A.cendColumns(pivotHelper.pivotRow(i));
       for (auto c = begin; c != end; ++c)
       {
-   	 if (pivotHelper.pivotColumn(i) != (*c).first && pivotHelper.isPivotColumn((*c).first))
+   	 if (pivotHelper.pivotColumn(i) != (*c) && pivotHelper.isPivotColumn(*c))
    	 {
    	    // if it is a pivot column, what is its pivot index?
-   	    VertexType j = pivotHelper.columnPivotIndex((*c).first);
+   	    VertexType j = pivotHelper.columnPivotIndex(*c);
 	    edges.push_back(std::make_pair(i,j));
    	 }
       }
    }
-   std::cout << "Number of edges: " << edges.size() << std::endl;
    initialize(edges);
 }
 
