7ad573a690255ae4a0dece0606628727a9a9e694
moorewf <moorewf@wfu.edu>
2023-01-04 18:12:10 -0500

    Added/moved some comments.

--- M2/Macaulay2/e/sparse-matrix/MatrixOps.hpp
+++ M2/Macaulay2/e/sparse-matrix/MatrixOps.hpp
@@ -8,10 +8,29 @@ class SparseMatrixZZp;
 class SparseVector;
 class DenseVector;
 
+// TODO: Go Through These, brought over from SparseMatrixZZp header
+
+  // Constructors, change of representation
+  // read/write to/from file
+
+  // Transpose
+  // Matrix/Vector Multiplication (v*A, A*B, ...)
+
+  // sparse PLUQ decomposition:
+  // Wiedemann
+  // Lanczos
+  // rank computation
+
+  // finding a good matching...
+
 namespace MatrixOps
 {
-  void upperUnitriangularSolve(const SparseMatrixZZp& A, const SparseVector& sparse, DenseVector& result);
-  void upperUnitriangluarSolve(const SparseMatrixZZp& A, const SparseVector& sparse, SparseVector& result);
+  // solves result*A = a.  Requires A to be a block 2x2 matrix with a upper unitriangular matrix in
+  // top left corner.
+  void upperUnitriangluarSolve(const SparseMatrixZZp& A, const SparseVector& a, SparseVector& result);
+  void upperUnitriangularSolve(const SparseMatrixZZp& A, const SparseVector& a, DenseVector& result);
+  void upperUnitriangluarSolve(const SparseMatrixZZp& A, const DenseVector& a, SparseVector& result);
+  void upperUnitriangularSolve(const SparseMatrixZZp& A, const DenseVector& a, DenseVector& result);
 
   // performs result += cx.  Scatters cx into result
   void scatter(const SparseMatrixZZp& A, const ZZpElement& element, const SparseVector& x, SparseVector& result);
@@ -24,6 +43,13 @@ namespace MatrixOps
   void rowUpdate(const SparseVector& sparse, SparseVector& result, DenseVector& b);
   void rowUpdate(const SparseVector& sparse, DenseVector& result, SparseVector& b);
   void rowUpdate(const SparseVector& sparse, DenseVector& result, DenseVector& b);
+
+  // What about: A+B, A-B, A += c*D, A*B, etc...
+
+  void schurComplement(const SparseMatrix& A, SparseMatrix& result);
+
+  IndexType rank(const SparseMatrix& A);
+  
 }
 
 #endif
