627163cba092134f7170fe4544c98fcf40272f99
Frank Moore <moorewf@wfu.edu>
2025-02-26 15:32:06 -0500

    Added (empty) BasicMatrix class for future development.

---
+++ M2/Macaulay2/e/BasicMatrix.hpp
@@ -0,0 +1,16 @@
+#pragma once
+
+// (pure?) virtual base class for other matrix types
+class BasicMatrix : public EngineObject
+{
+public:
+   
+}
+
+// Local Variables:
+// compile-command: "make -C $M2BUILDDIR/Macaulay2/e "
+// indent-tabs-mode: nil
+// End:
+
+
+  
